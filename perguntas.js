5e5c2ffa74b9ea18b4fc902c5cb65ff6c41d80f2
ivasitinha <maria.ivasita@escola.pr.gov.br>
2025-06-24 09:36:31 -0300

    salve

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é Javascript?',
+    'O javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Paraná',
+    'A capital do Paraná é Curitiba'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
